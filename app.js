e1069117ffca3a13092cc464d6412f6e42765848
MaykeSN <mayke.souza2002@gmail.com>
2025-03-09 13:10:23 -0300

    feat: adicionado modal para erros ou conclusoes no sistema

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-class Expense {
-    constructor(ano, mes, dia, tipo, descricao, valor){
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.tipo = tipo;
-        this.descricao = descricao;
-        this.valor = valor;
-    }
-}
+
+document.getElementById('register').addEventListener('click', registerExpense);
 
 function registerExpense(){
     let ano = document.getElementById("ano").value;
@@ -18,10 +10,69 @@ function registerExpense(){
     let valor = document.getElementById("valor").value;
 
     const expense = new Expense(ano, mes, dia, tipo, descricao, valor);
-    save(expense);
-    
+    if(expense.validate())
+    {
+        let bd = new BD();
+        bd.save(expense); 
+        document.getElementById(`modal_title`).innerHTML = 'Sucesso';
+        document.getElementById(`modal_content`).innerHTML = 'Despesa adicionada com sucesso!';
+        document.getElementById(`modal_btn`).innerHTML = 'Voltar';
+        document.getElementById(`modal_header`).className = 'modal-header text-success';
+        document.getElementById(`modal_btn`).className = 'btn btn-success';
+        $("#sucessoGravacao").modal('show');
+    }
+    else{
+        document.getElementById(`modal_title`).innerHTML = 'Erro na inclusao do registro';
+        document.getElementById(`modal_content`).innerHTML = 'Ha campos obrigatorios que nao foram preenchidos';
+        document.getElementById(`modal_btn`).innerHTML = 'Voltar e corrigir';
+        document.getElementById(`modal_header`).className = 'modal-header text-danger';
+        document.getElementById(`modal_btn`).className = 'btn btn-danger';
+        $("#sucessoGravacao").modal('show');
+    }
 }
 
-function save(expense){
-    localStorage.setItem('expense', JSON.stringify(expense))
+class Expense {
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    validate(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i] == null){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class BD {
+
+    constructor(){
+        let id = localStorage.getItem('id');
+
+        if(id === null){
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getNextId(){
+        let nextId = localStorage.getItem('id');
+
+        var nextNumber = parseInt(nextId) + 1;
+        return nextNumber;
+    }
+
+    save(expense){
+
+        var id = this.getNextId();
+        localStorage.setItem('id', id);
+        localStorage.setItem(id , JSON.stringify(expense))
+    }
 }
